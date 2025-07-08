284e9aff9e45c6bc77f55ac7be862318d1bc7320
Matheus Oliveira <matheus-homem2001@hotmail.com>
2025-07-07 23:21:17 -0300

    Page not found

--- pizza_shop-web/src/routes.tsx
+++ pizza_shop-web/src/routes.tsx
@@ -6,12 +6,14 @@ import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { SignUp } from "./pages/auth/sign-up";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
 
   {
     path: '/', 
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path : '/home', element: <Dashboard /> },
       { path : '/orders', element: <Orders /> }
