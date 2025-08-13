451a57c5b81e802d8a24771e438aa90512713fc7
Matheus Oliveira <matheus-homem2001@hotmail.com>
2025-08-12 22:04:36 -0300

    Initializing tests

--- pizza_shop-web/src/components/order-status.tsx
+++ pizza_shop-web/src/components/order-status.tsx
@@ -23,19 +23,19 @@ export function OrderStatus({ status }: OrderStatusProps){
   return(
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
+        <span data-testid="badge" className="h-2 w-2 rounded-full bg-slate-400" />
       )}
 
       {['delivering', 'processing'].includes(status) && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+        <span data-testid="badge" className="h-2 w-2 rounded-full bg-amber-500" />
       )}
 
       {status === 'canceled' && (
-        <span className="h-2 w-2 rounded-full bg-red-500" />
+        <span data-testid="badge" className="h-2 w-2 rounded-full bg-red-500" />
       )}
 
       {status === 'delivered' && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        <span data-testid="badge" className="h-2 w-2 rounded-full bg-emerald-500" />
       )}
       
       <span className="font-medium text-muted-foreground">{orderStatusMap[status]}</span>
--- pizza_shop-web/vite.config.ts
+++ pizza_shop-web/vite.config.ts
@@ -1,14 +1,18 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
+import path from 'path'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom'
   },
 })
